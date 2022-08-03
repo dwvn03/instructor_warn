6cb14ffbfe0761d020d562f5fe972eda432a7686
Lộc Đỗ <locpro181103@gmail.com>
2022-08-03 16:07:26 +0700

    v0.0.4: concurrent python client

---
+++ client/src/App.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+import io from "socket.io-client";
+
+import { newNotif, askNotificationPermission } from "./notif";
+
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+const socket = io(BASE_URL);
+
+function App() {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [lastPong, setLastPong] = useState(null);
+  const [value, setValue] = useState("");
+
+  useEffect(() => {
+    askNotificationPermission();
+
+    socket.on("connect", () => {
+      setIsConnected(true);
+    });
+
+    socket.on("disconnect", () => {
+      setIsConnected(false);
+    });
+
+    socket.on("warned", (text) => {
+      setLastPong(new Date().toISOString() + " " + text);
+      newNotif("Be Careful", "Your instructor is looking at you");
+    });
+
+    return () => {
+      socket.off("connect");
+      socket.off("disconnect");
+      socket.off("warned");
+    };
+  }, []);
+
+  const sendPing = () => {
+    socket.emit("ping");
+  }
+
+  const sendPingWithText = () => {
+    socket.emit("ping", value);
+  }
+
+  return (
+    <div>
+      <p>Connected: { "" + isConnected }</p>
+      <p>Last pong: { lastPong || "-" }</p>
+      <button onClick={ sendPing }>Send ping</button>
+      <input type="text" onChange={(e) => setValue(e.target.value) } />
+      <button onClick={ sendPingWithText }>Ping text</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/notif.js
@@ -0,0 +1,38 @@
+navigator.serviceWorker.register("sw.js");
+Notification.requestPermission(result => {
+	if (result === "granted") {
+		navigator.serviceWorker.ready.then(function(registration) {
+			registration.showNotification("Notification with ServiceWorker enabled");
+		});
+	} else {
+		alert("This app requires notification permission to work properly");
+	}
+});
+
+export function newNotif(title, text) {
+	const content = {
+		body: text
+	};
+
+	navigator.serviceWorker.ready.then(registration => {
+		registration.showNotification(title, content);
+	});
+
+	setTimeout(() => {
+		navigator.serviceWorker.ready.then(registration => {
+			registration.getNotifications().then(notifications => {
+				for (let notif of notifications) {
+					notif?.close()
+				}
+			})
+		});
+	}, 9900);
+}
+
+export function askNotificationPermission() {
+	if (!("Notification" in window)) {
+		console.log("This browser does not support notifications.");
+	} else {
+		Notification.requestPermission();
+	}
+}
